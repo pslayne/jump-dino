da19aa3db9fae21b57ba7898191d2e436e1aa02c
pslayne <58180438+pslayne@users.noreply.github.com>
2020-12-16 18:28:27 -0300

    Add files via upload

--- jumpdino.asm
+++ jumpdino.asm
@@ -9,7 +9,7 @@
 #	Unit Height: 8						     #
 #	Display Width: 512					     #
 #	Display Height: 256					     #
-#	Base Address for Display: 0x10040000 (heap)	             #
+#	Base Address for Display: 0x10008000 ($gp)	             #
 #	                                                             #
 #                                                                    #
 #                                                                    #
@@ -29,11 +29,20 @@ zero: .word 0x00000000
 score: .word 0
 vidas: .word 3
 
+msg: .asciiz "que pena, você perdeu... sua pontuação foi: "
+msgr: .asciiz "deseja recomeçar? 1 para sim e 2 para não"
+
 #endereço
-screenStart: .word 0x10040000 #a tela começa na heap
+screenStart: .word 0x10008000 #a tela começa em $gp
 
 .macro Fim
 #finaliza o programa
+	la $a0, msg
+	li $v0, 4
+	syscall
+	move $a0, $t9
+	li $v0, 1
+	syscall
         li $v0, 10
         syscall
 .end_macro
@@ -71,24 +80,24 @@ screenStart: .word 0x10040000 #a tela começa na heap
 	li $t0, 256
 	li $t1, 0
 	lw $t3, screenStart #t3 recebe o endereço inicial da tela
-	lw $t2, fundo #t2 recebe a cor do fundo
+	lw $t4, fundo #t2 recebe a cor do fundo
 	
 	desenhaLinha:
-		sw $t2, ($t3) #faz o store da cor de fundo em t3
+		sw $t4, ($t3) #faz o store da cor de fundo em t3
 		addi $t3, $t3, 4 #incrementa o endereço 
 		addi $t1, $t1, 4 #incrementa o contador
 		blt $t1, $t0, desenhaLinha
 		b proxLinha
 	
 	desenhaChao:
-		lw $t2, cacto
+		lw $t4, cacto
 		
 		desenhaChaoLinha:
-			sw $t2, ($t3) #faz o store da cor do cacto em t3
+			sw $t4, ($t3) #faz o store da cor do cacto em t3
 			addi $t3, $t3, 4 #incrementa o endereço
 			addi $t1, $t1, 4 #incrementa o contador para saber quando trocar de linha
 			blt $t1, $t0, desenhaChaoLinha
-			lw $t2, fundo
+			lw $t4, fundo
 			b proxLinha
 			
 	proxLinha:
@@ -103,10 +112,10 @@ screenStart: .word 0x10040000 #a tela começa na heap
 	li $t0, 256
 	li $t1, 0
 	lw $t3, screenStart #t3 recebe o endereço de início da tela
-	lw $t2, cacto #t2 recebe a cor do cacto
+	lw $t4, cacto #t2 recebe a cor do cacto
 	
 	desenhaLinha:
-		sw $t2, ($t3) #faz o store da cor para o endereço em t3
+		sw $t4, ($t3) #faz o store da cor para o endereço em t3
 		addi $t3, $t3, 4 #incrementa o endereço
 		addi $t1, $t1, 4 #incrementa o contador
 		SleepShorter
@@ -114,14 +123,14 @@ screenStart: .word 0x10040000 #a tela começa na heap
 		b proxLinha
 	
 	desenhaChao:
-		lw $t2, cacto
+		lw $t4, cacto
 		
 		desenhaChaoLinha:
-			sw $t2, ($t3) #faz o store da cor para o endereço em t3
+			sw $t4, ($t3) #faz o store da cor para o endereço em t3
 			addi $t3, $t3, 4 #incrementa o endereço 
 			addi $t1, $t1, 4 #incrementa o contador para saber quando mudar de linha
 			blt $t1, $t0, desenhaChaoLinha
-			lw $t2, cacto
+			lw $t4, cacto
 			b proxLinha
 			
 	proxLinha:
@@ -149,7 +158,13 @@ screenStart: .word 0x10040000 #a tela começa na heap
 .macro checarColisao
 #a colisão acontece se o bit que vai ser desenhado tem a cor do dinossauro
 lw $t0, ($s3)
-beq $t0, 0x8B0000, gameOver
+bne $t0, 0x8B0000, score
+
+addi $t8, $t8, -1
+
+beqz $t8, gameOver
+
+score: add $t9, $t9, 100	
 .end_macro
 
 .macro cactus1Left
@@ -161,16 +176,23 @@ beq $t0, 0x8B0000, gameOver
 	#para de desenhar quando chega ao fim da tela
 	addi $s1, $s1, 1
 	beq $s1, 62, novoCacto
+	
 	Sleep
 .end_macro 
+
 .text
+reset:
 desenha
+
+li $t9, 0 #salva a pontuação
+li $t8, 3 #salva as vidas
 
 li $a1, 0xFFFF0004 #salva o local da entrada do teclado em a1
 
 jal inicioDino
 jal inicioCacto1
 
+syscall
 
 displayLoop:
 	lw $t7, ($a1) #carrega a entrada pra t7
@@ -194,7 +216,7 @@ inicioDino:
 	
 desenhaDino:
 	li $t1, 0 #reseta o contador horizontal
-	li $t2, 0 #reseta o contador vertical
+	li $t0, 0 #reseta o contador vertical
 	
 	li $t4, 3 #largura máxima do corpo
 	li $t7, 3 #altura máxima do corpo
@@ -217,12 +239,12 @@ desenhaDino:
 		sub $s3, $s3, $t5
 		subi $s3, $s3, 256
 		li $t1, 0 # reset the horizontal counter
-		addi $t2, $t2, 1
-		blt $t2, $t7, desenhaCorpoHor
+		addi $t0, $t0, 1
+		blt $t0, $t7, desenhaCorpoHor
 		
 		li $t4, 5 # maximum head width
 		li $t5, 20 # how much to move back for each level with head
-		blt $t2, $t6, desenhaCorpoHor
+		blt $t0, $t6, desenhaCorpoHor
 		j desenhaCauda
 		
 	desenhaCauda:
@@ -240,7 +262,7 @@ desenhaDino:
 
 apagaDino:
 	li $t1, 0 #reseta o contador horizontal
-	li $t2, 0 #reseta o contador vertical
+	li $t0, 0 #reseta o contador vertical
 	
 	li $t4, 3 #largura máxima do corpo
 	li $t7, 3 #altura máxima do corpo
@@ -263,12 +285,12 @@ apagaDino:
 		sub $s3, $s3, $t5
 		subi $s3, $s3, 256
 		li $t1, 0 # reset the horizontal counter
-		addi $t2, $t2, 1
-		blt $t2, $t7, apagaCorpoHor
+		addi $t0, $t0, 1
+		blt $t0, $t7, apagaCorpoHor
 		
 		li $t4, 5 # maximum head width
 		li $t5, 20 # how much to move back for each level with head
-		blt $t2, $t6, apagaCorpoHor
+		blt $t0, $t6, apagaCorpoHor
 		j apagaCauda
 		
 	apagaCauda:
@@ -414,6 +436,7 @@ novoCacto:
 	jal apagaCacto1
 	jal inicioCacto1
 	addi $s0, $s0, 1
+	
 	j displayLoop
 	
 jumpBack:
@@ -423,23 +446,29 @@ jumpingDisplayLoop:
 	lw $t0, zero #refresh da entrada do teclado
 	sw $t0, ($a1)
 	
-	li $t8, 0 #começa o contador
+	li $t1, 0 #começa o contador
 	upLoop:
 		dinoUp
 		cactus1Left
 		
-		addi $t8, $t8, 1
-		bne $t8, 12, upLoop
+		addi $t1, $t1, 1
+		bne $t1, 12, upLoop
 	
-	li $t8, 0 #começa o contador
+	li $t1, 0 #começa o contador
 	downLoop:
 		dinoDown
 		cactus1Left
 		
-		addi $t8, $t8, 1
-		bne $t8, 12, downLoop
+		addi $t1, $t1, 1
+		bne $t1, 12, downLoop
 	j displayLoop
 	
 gameOver:
 	finaliza
+	la $a0, msgr
+	li $v0, 4
+	syscall
+	li $v0, 5
+	syscall
+	beq $v0, 1, reset
 	Fim
